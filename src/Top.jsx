898ba1f07bd92b66c4232b5efcdcd0f9124de725
lalitsecret <lalitsecret@gmail.com>
2021-03-19 12:41:30 +0530

    3 files modified

--- src/Top.jsx
+++ src/Top.jsx
@@ -2,7 +2,29 @@ import React from "react";
 function App()
 {
 	return <div className="top">
-		top
+		<div className="item">
+			<strong>quick search</strong>
+			<div className="input">
+				<i className="fa fa-search"></i>
+				<input placeholder="search..." />
+			</div>
+		</div>
+		<div className="item">
+			<div>	
+				<button>by date</button>
+				<button>by period</button>
+			</div>
+			<div>
+				<select>
+					<option value="">Quick Filter</option>
+				</select>
+				<input type="date"/>
+				<input type="date"/>
+				<button><i className="fa fa-print"></i></button>
+				<button><i className="fa fa-file"></i></button>
+				<button><i className="fa fa-cog"></i></button>
+			</div>
+		</div>
 	</div>
 }
 export default App
